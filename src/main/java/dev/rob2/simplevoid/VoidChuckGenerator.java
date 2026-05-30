d5a6100c6f2025d168921c9e93cc2f39aaf2a6db
Robert Turner <rob@rob2.dev>
2026-05-29 19:02:41 -0700

    Added CreateVoidCommand, VoidChunkGenerator

---
+++ src/main/java/dev/rob2/simplevoid/VoidChuckGenerator.java
@@ -0,0 +1,15 @@
+package dev.rob2.simplevoid;
+
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
+
+public class VoidChunkGenerator extends ChunkGenerator {
+
+    @Override
+    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
+        return createChunkData(world); // empty chunk
+    }
+}
+
